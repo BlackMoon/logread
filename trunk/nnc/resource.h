00d08f812228224fa3b4e2f45d0c1d7b11a2685c
rrv.kazan@gmail.com <rrv.kazan@gmail.com@df9588ce-b55d-d2bd-40ee-67013405feb9>
2011-11-29 11:13:45 +0000

    nnc

---
+++ trunk/nnc/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by nnc.rc
+//
+#define IDD_NNC                         101
+#define IDE_Z1                          1001
+#define IDE_Z2                          1002
+#define IDE_TRANS                       1003
+#define IDE_TRAN                        1003
+#define IDE_NX                          1004
+#define IDE_NY                          1005
+#define ID_RUN                          1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
